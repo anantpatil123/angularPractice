81bebf3071dead23acc7c013ca1ad8cb28da162b
anant Patil <ant41107@gmail.com>
2024-07-03 01:50:17 +0530

    1. Login-page basic UI is created 2. prime ng is added intoproject 3.ionic framework is added into project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
+import { IonicModule } from '@ionic/angular';
+import { ButtonModule } from 'primeng/button';
+import { LoginModule } from './pages/login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
     PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IonicModule.forRoot({}),
+    ButtonModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { LoginRoutingModule } from './login-routing.module';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { LoginComponent } from './login.component';
+import { IonicModule } from '@ionic/angular';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
-  ]
+    LoginRoutingModule,
+    InputTextModule,
+    ButtonModule,
+    IonicModule
+  ],exports:[LoginComponent]
 })
 export class LoginModule { }
